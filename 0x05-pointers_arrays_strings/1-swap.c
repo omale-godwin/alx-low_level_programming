3eb6a47580a6ff61d7e76ceaa1229185cd0138dc
omale-godwin <godwinpeace3@gmail.com>
2023-02-27 11:06:01 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * 1-swap.c -swaps the values of two integers.
+ * second code 
+ * @n: void swap_int
+ **/
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = *b;
+*b = *a;
+*a = c;
+}
