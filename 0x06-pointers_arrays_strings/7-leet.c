14306b59a18d7e5b0d1565fd3cfc2f63929cd192
omale-godwin <godwinpeace3@gmail.com>
2023-03-02 08:26:53 -0800

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,30 @@
 #include "main.h"
 /**
- * leet - encodes a string into 1337
- * @s: input variable string. 
- * Return: the pointer to dest.
+ * leet - function to encodes a string into 1337
+ * @s: inputed  string. 
+ * Return: the  dest.
  */
 
 char *leet(char *s)
 {
-	int y = 0, i;
-	int lows[] = {97, 101, 111, 116, 108};
-	int upps[] = {65, 69, 79, 84, 76};
-	int numbs[] = {52, 51, 48, 55, 49};
+        int y = 0, i;
+        int lows[] = {97, 101, 111, 116, 108};
+        int upps[] = {65, 69, 79, 84, 76};
+        int numbs[] = {52, 51, 48, 55, 49};
 
-	while (*(s + y) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + y) == lows[i] || *(s + y) == ups[i])
-			{
-				*(s + y) = numbs[i];
-				break;
-			}
-		}
-		y++;
-	}
+        while (*(s + y) != '\0')
+        {
+                for (i = 0; i < 5; i++)
+                {
+                        if (*(s + y) == lows[i] || *(s + y) == ups[i])
+                        {
+                                *(s + y) = numbs[i];
+                                break;
+                        }
+                }
+                y++;
+        }
 
-	return (s);
+        return (s);
 }
+   
