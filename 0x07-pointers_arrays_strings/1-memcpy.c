51223e0732999a5ac128ef2f1ac8502c07b3786a
omale-godwin <godwinpeace3@gmail.com>
2023-03-06 06:03:31 -0800

    2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _memcpy - function that fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: lenght of buffer
+ * Return: new string.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i;
+		i++;
+	}
+	return (dest);
+}
