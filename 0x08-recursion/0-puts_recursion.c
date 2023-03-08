a289bd0e8bdc06d35ae3e47f08cfff3b77d5d82b
omale-godwin <godwinpeace3@gmail.com>
2023-03-08 10:53:03 -0800

    000

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-
 /**
- * main - check the code
- @*s - the vairable string pass
- * Return: Always 0.
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+	_putchar('\n');
+	}
 }
-
