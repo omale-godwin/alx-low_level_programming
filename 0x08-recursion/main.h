42518cfd8d5341a9d009f819ecb15324c51706a4
omale-godwin <godwinpeace3@gmail.com>
2023-03-09 11:30:09 -0800

    advance trying

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _sqrt_recursion(int n);
-	int is_prime_number(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
