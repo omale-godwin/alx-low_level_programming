501135b9134e201cbaa7e513e3b20dba94236db4
omale-godwin <godwinpeace3@gmail.com>
2023-03-14 12:06:28 -0700

    number one again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
-/**
- * main - program startup
- * @argc: argument count
- * @argv: argument vector
-(*
- * Description: prints its own program name followed by a new line)?
- * Return: return (0) is the required function signature
- */
 
-#include "main.h"
+
+#include <stdio.h>
+
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	argc = argc;
+	printf("%s\n", *argv);
 	return (0);
 }
