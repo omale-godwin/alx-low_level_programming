7be54ffbff5092c50f3fe434efa76eac656f76c8
omas1 <godwinomale2020@gmail.com>
2023-03-31 19:43:31 +0200

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,47 @@
+// #include <stdio.h>
+// #include "lists.h"
+
+// /**
+//  * print_list - prints all the elements of a list_t list
+//  * @h: pointer to a constant structure list_t
+//  *
+//  * Return: number of nodes in the list
+//  */
+// size_t print_list(const list_t *h)
+// {
+//     size_t num_nodes = 0;
+
+//     if (h == NULL)
+//         return (0);
+
+//     if (h->str == NULL)
+//         printf("[0] (nil)\n");
+//     else
+//         printf("[%zu] %s\n", h->len, h->str);
+
+//     num_nodes++;
+
+//     while (h->next != NULL)
+//     {
+//         h = h->next;
+
+//         if (h->str == NULL)
+//             printf("[0] (nil)\n");
+//         else
+//             printf("[%zu] %s\n", h->len, h->str);
+
+//         num_nodes++;
+//     }
+
+//     return (num_nodes);
+// }
+
 #include <stdio.h>
 #include "lists.h"
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: pointer to a constant structure list_t
+ * @h: pointer to the head of the list
  *
  * Return: number of nodes in the list
  */
@@ -11,25 +49,14 @@ size_t print_list(const list_t *h)
 {
     size_t num_nodes = 0;
 
-    if (h == NULL)
-        return (0);
-
-    if (h->str == NULL)
-        printf("[0] (nil)\n");
-    else
-        printf("[%zu] %s\n", h->len, h->str);
-
-    num_nodes++;
-
-    while (h->next != NULL)
+    while (h != NULL)
     {
-        h = h->next;
-
         if (h->str == NULL)
             printf("[0] (nil)\n");
         else
-            printf("[%zu] %s\n", h->len, h->str);
+            printf("[%d] %s\n", h->len, h->str);
 
+        h = h->next;
         num_nodes++;
     }
 
