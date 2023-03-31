62423e827ba9cba0efb6d99ff11fb22a8b4e6765
omas1 <godwinomale2020@gmail.com>
2023-03-31 19:41:30 +0200

    task correlation

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include "lists.h"
 
-
 /**
- * print_list - these function that prints all the elements of a list_t list
- * @h: Constant varaible to pointer of structure list_t
- * Return: Number of nodes available
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to a constant structure list_t
+ *
+ * Return: number of nodes in the list
  */
-
-
 size_t print_list(const list_t *h)
 {
-	unsigned int p = 0;
+    size_t num_nodes = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+    if (h == NULL)
+        return (0);
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
+    if (h->str == NULL)
+        printf("[0] (nil)\n");
+    else
+        printf("[%zu] %s\n", h->len, h->str);
 
-		h = h->next;
-		p++;
-	}
+    num_nodes++;
 
-	return (p);
+    while (h->next != NULL)
+    {
+        h = h->next;
+
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%zu] %s\n", h->len, h->str);
+
+        num_nodes++;
+    }
+
+    return (num_nodes);
 }
