7a92f41dae34dee1371dfd5da1d8f0512e60ede5
omas1 <godwinomale2020@gmail.com>
2023-04-01 20:26:51 +0200

    task o aga

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,30 @@
-#include <stdio.h>
+
+
 #include "lists.h"
+#include <stdio.h>
+
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: the number of nodes printed
+ * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-const list_t *current = h;
+	size_t node = 0;
 
-while (current != NULL)
-{
-	if (current->str != NULL)
+	while (h)
 	{
-		printf("[%d] %s\n",current->len, current->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		node++;
+		h = h->next;
 	}
-	else
-	{
-		printf("[0] (nil)\n");
-	}
-	count++;
-	current = current->next;
-}
-return (count);
+
+	return (node);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,8 +3,9 @@
 
 /**
  * free_list - frees a linked list
- * @head: list_t list to be freed
+ * @head: thes  list_t list to be freed
  */
+ 
 void free_list(list_t *head)
 {
  list_t *tmp;
