1d20ab415979e2be1cc96294a2de614399ce4761
omas1 <godwinomale2020@gmail.com>
2023-04-03 19:39:29 +0200

    first task o

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - these will actually prints all the elements of a list.
+ * @h: variable head of a list.
+ *
+ * Return: returning the numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t ndes = 0;
+
+	for (; h != NULL; h = h->next)
+    {
+     printf("%d\n", h->n);
+      ndes++;
+}
+	return (ndes);
+}
