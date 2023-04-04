2e258610e72202fd815e2fed78ea2293e6020b85
omas1 <godwinomale2020@gmail.com>
2023-04-04 13:57:12 +0200

    tasl

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * listint_len - Returns the numbs of elements in a linked listint_t list.
+ * listint_len - Returns the number of elements in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
- * Return: The numbs of elements in the listint_t list.
+ * Return: The number of elements in the listint_t list.
  */
 size_t listint_len(const listint_t *h)
 {
