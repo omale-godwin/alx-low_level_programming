ff17a61a88746fa4e9f8f1a3a1c37276878eb5ad
omas1 <godwinomale2020@gmail.com>
2023-04-08 16:33:56 +0200

    question 4

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+        unsigned int intg;
+        int length, base2;
+
+        if (!b)
+                return (0);
+
+        intg = 0;
+
+        for (length = 0; b[length] != '\0'; length++)
+                ;
+
+        for (length--, base2 = 1; length >= 0; length--, base2 *= 2)
+        {
+                if (b[length] != '0' && b[length] != '1')
+                {
+                        return (0);
+                }
+
+                if (b[length] & 1)
+                {
+                        intg += base2;
+                }
+        }
+
+        return (intg);
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - funtion to print the code
+ * @c: char variable 0.
+ *
+ * Return On success 1.
+ * on error -1 is return
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
