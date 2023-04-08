51e50ebc8f8228cf69a335572ce179e73fadaf71
omale-godwin <godwinpeace3@gmail.com>
2023-04-08 06:22:52 -0700

    1 and 2 question

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,16 @@
  * get_bit - gets bit on index `index`
  * @n: decimal number
  * @index: index of the bit
- * Return: the bit found
+ * Return: the bit values
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * BYTE_LENGHT)
-		return (-1);
-
-	return ((n >> index) & 1);
+int bitwise_value;
+if (index > 63)
+	{
+	return (-1);
+	}
+bitwise_value = (n >> index) & 1;
+return (bitwise_value);
 }
