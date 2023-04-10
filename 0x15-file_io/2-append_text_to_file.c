31e5ee3067a6b3228b15dc610c8d91abf51db0cf
omas1 <godwinomale2020@gmail.com>
2023-04-10 19:47:34 +0200

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,35 +14,36 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
-	ssize_t text_len;
-	ssize_t bytes_written;
-    if (filename == NULL) {
-        return (-1);
-    }
+int file;
+ssize_t text_len;
+ssize_t bytes_written;
+if (filename == NULL)
+{
+	return (-1);
+}
 
-    file = open(filename, O_WRONLY | O_APPEND);
-    if (file == -1)
+file = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
+{
+	return (-1);
+}
+
+if (text_content != NULL)
+{
+	text_len = strlen(text_content);
+	bytes_written = write(file, text_content, text_len);
+	if (bytes_written != text_len)
 	{
-        return (-1);
-    }
+		close(file);
+		return (-1);
+	}
+}
 
-    if (text_content != NULL)
-	{
-        text_len = strlen(text_content);
-        bytes_written = write(file, text_content, text_len);
-        if (bytes_written != text_len)
-		{
-            close(file);
-            return (-1);
-        }
-    }
+if (close(file) == -1)
+{
+	return (-1);
+}
 
-    if (close(file) == -1)
-	{
-        return (-1);
-    }
-
-    return (1);
+return (1);
 }
 
