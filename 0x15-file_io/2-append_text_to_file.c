e3b47583a00cebcab772032ddb7ba0bf0750f409
omas1 <godwinomale2020@gmail.com>
2023-04-10 19:45:48 +0200

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 /**
  * append_text_to_file - Append  end of a file
@@ -8,24 +14,35 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_data, status, i;
+	int file;
+	ssize_t text_len;
+	ssize_t bytes_written;
+    if (filename == NULL) {
+        return (-1);
+    }
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
+    file = open(filename, O_WRONLY | O_APPEND);
+    if (file == -1)
+	{
+        return (-1);
+    }
 
-	file_data = open(filename, O_APPEND | O_WRONLY);
-	if (file_data == -1)
-		return (-1);
+    if (text_content != NULL)
+	{
+        text_len = strlen(text_content);
+        bytes_written = write(file, text_content, text_len);
+        if (bytes_written != text_len)
+		{
+            close(file);
+            return (-1);
+        }
+    }
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(file_data, text_content, i);
-	if (status == -1)
-		return (-1);
+    if (close(file) == -1)
+	{
+        return (-1);
+    }
 
-	close(file_data);
-	return (1);
+    return (1);
 }
 
