e944f8e068a2b8a374a4365656ac6cd2b707d41b
omas1 <godwinomale2020@gmail.com>
2023-04-12 11:16:10 +0200

    task 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,55 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
-
 /**
- * read_textfile - reads file and prints to the POSIX  output
- * @filename: pointer to the file
- * @letters: number of letters it read and print
- * Return:  the actual number of letters it read and print
-*/
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-FILE *filed;
-char *buffered;
-ssize_t bytes_to_read;
-ssize_t bytes_written;
+	char *buffer;
+	ssize_t f_d;
+	ssize_t w;
+	ssize_t t;
 
-if (filename == NULL)
-{
-	return (0);
-}
-filed = fopen(filename, "r");
-if (filed == NULL)
-{
-	return (0);
-}
-buffered = malloc(sizeof(char) * (letters + 1));
-if (buffered == NULL)
-{
-	fclose(filed);
-	return (0);
-}
-bytes_to_read = fread(buffered, sizeof(char), letters, filed);
-if (bytes_to_read <= 0)
-{
-fclose(filed);
-free(buffered);
-return (0);
-}
+	f_d = open(filename, O_RDONLY);
+	if (f_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	t = read(f_d, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
 
-bytes_written = write(STDOUT_FILENO, buffered, bytes_to_read);
-if (bytes_written != bytes_to_read)
-{
-fclose(filed);
-free(buffered);
-return (0);
-}
-
-fclose(filed);
-free(buffered);
-return (bytes_to_read);
+	free(buffer);
+	close(f_d);
+	return (w);
 }
