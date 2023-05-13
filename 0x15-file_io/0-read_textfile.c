b5b5f62ff6f9e6aaa0db4ea086c46e1dab67adc3
omale-godwin <godwinpeace3@gmail.com>
2023-05-12 23:45:47 -0700

    o read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,57 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the text file to be read
+ * @letters: Number of letters that should be read and printed
+ *
+ * Return: Actual number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	ssize_t f_d;
-	ssize_t w;
-	ssize_t t;
+	int bytes_read, bytes_written, file_desc;
+	char *buff;
 
-	f_d = open(filename, O_RDONLY);
-	if (f_d == -1)
+	if (!filename)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	t = read(f_d, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, t);
 
-	free(buffer);
-	close(f_d);
-	return (w);
+	buff = malloc(sizeof(char) * letters + 1); /* Allocate buff for reading */
+	if (!buff)
+		return (0);
+
+	file_desc = open(filename, O_RDONLY); /* Open file for reading only */
+	if (file_desc == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	bytes_read = read(file_desc, buff, letters); /* Read file into buffer */
+	if (bytes_read == -1)
+	{
+		free(buff);
+		close(file_desc);
+		return (0);
+	}
+
+	/* Write the buffer to stdout */
+	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
+	if (bytes_written == -1 || ((size_t)bytes_written != (size_t)bytes_read))
+	{
+		free(buff);
+		close(file_desc);
+		return (0);
+	}
+
+	/* After writing to terminal, free buffer and close file descriptor */
+	free(buff);
+	close(file_desc);
+
+	return (bytes_written);
 }
 
